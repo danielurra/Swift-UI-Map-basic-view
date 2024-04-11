d82bd6ac0e61c4f3b555e36bfade6b8dd2ce6056
Daniel Urra <danielurra75@gmail.com>
2024-04-11 04:34:42 -0500

    Create map-basic-view.swift

---
+++ map-basic-view.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FirstMapsApp
+//
+//  Created by Eusebio Taba on 4/10/24.
+//
+
+import SwiftUI
+import UIKit
+import MapKit
+
+struct ContentView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    ContentView()
+}
